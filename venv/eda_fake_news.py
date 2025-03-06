20c71e1f59573efb6ecdb4be04d9f6f278fd8018
SotirovaSofia <sofiasotirova@gmail.com>
2025-03-06 18:55:42 +0100

    Subiendo requirements 2

---
+++ venv/eda_fake_news.py
@@ -0,0 +1,8 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+from wordcloud import WordCloud
+import nltk
+from nltk.corpus import stopwords
+from nltk.tokenize import word_tokenize
